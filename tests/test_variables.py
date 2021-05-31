d3cf2259498f20ac76de7e50553ae62c750ffd8c
katepechekhonova <ekate@nyu.edu>
2021-05-31 15:51:47 -0400

    Modified e2e and config tests

--- tests/test_variables.py
+++
@@ -1,9 +0,0 @@
-import os
-from unittest import mock
-from invenio_app.factory import create_app
-
-@mock.patch.dict(os.environ, {"APP_ALLOWED_HOSTS": "ultraviolet.dlib.nyu.edu"})
-def test_var():
-    app=create_app()
-    apple = app.config.get("APP_ALLOWED_HOSTS")
-    assert apple == "ultraviolet.dlib.nyu.edu"
