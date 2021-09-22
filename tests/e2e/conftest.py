52cde8ef3c14c6f56b72fecb39beae2b3b16224f
katepechekhonova <ekate@nyu.edu>
2021-09-21 22:33:08 -0400

    Updated tests configuration

---
+++ tests/e2e/conftest.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+#
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 NYU.
+#
+# invenio-nyu is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Pytest fixtures and plugins for the UI application."""
+
+# conftest.py
+"""Pytest configuration."""
+
+
+import invenio_app.factory as factory
+from invenio_base.wsgi import create_wsgi_factory, wsgi_proxyfix
+from invenio_config import create_config_loader
+import pytest
+import os
+
+
+@pytest.fixture(scope='module')
+def app_config(app_config):
+    app_config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        "pool_pre_ping": False,
+        "pool_recycle": 3600,
+    }
+    return app_config
+
+@pytest.fixture(scope='module')
+def create_app_e2e():
+    create_app_e2e = factory.create_app_factory(
+        'invenio',
+        config_loader=create_config_loader(config=None,env_prefix="Invenio"),
+        blueprint_entry_points=['invenio_base.blueprints'],
+        extension_entry_points=['invenio_base.apps'],
+        converter_entry_points=['invenio_base.converters'],
+        instance_path=os.getenv("INVENIO_INSTANCE_PATH"),
+        static_folder=os.getenv("INVENIO_INSTANCE_PATH")+"/static",
+        root_path=os.getenv("INVENIO_INSTANCE_PATH"),
+        wsgi_factory=wsgi_proxyfix(create_wsgi_factory({'/api': factory.create_api})),
+        static_url_path="/static",
+        app_class=factory.app_class()
+    )
+    return create_app_e2e
+
+
+@pytest.fixture(scope='module')
+def create_app(create_app_e2e):
+    """Flask app fixture."""
+    return create_app_e2e
--- tests/e2e/test_front_page.py
+++ tests/e2e/test_front_page.py
@@ -1,14 +1,13 @@
 """E2E test of the front page."""
 
 from flask import url_for
+from selenium.webdriver.common.by import By
 
 def test_frontpage(live_server, browser):
     """Test retrieval of front page."""
     browser.get(url_for('invenio_app_rdm.index', _external=True))
-    html = browser.page_source
-    print(html)
     assert (
             "UltraViolet"
-            == browser.find_element_by_tag_name("h1")
+            == browser.find_element(By.TAG_NAME, 'h1')
             .text
     )
