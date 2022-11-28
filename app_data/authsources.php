e8d2be14f9770112d3169b12aa2b873025408d85
AkshayUHegde <akshayudayhegde2@gmail.com>
2022-11-28 17:41:08 -0500

    Added Mock SAML Auth Sources

---
+++ app_data/authsources.php
@@ -0,0 +1,48 @@
+<?php
+
+$config = array(
+
+    'admin' => array(
+        'core:AdminPassword',
+    ),
+
+    'example-userpass' => array(
+        'exampleauth:UserPass',
+        'user1:user1pass' => array(
+            'uid' => 'nyu001',
+            'givenName' => 'Givenname1',
+            'surname' => 'Surname1',
+            'eduPersonPrincipleName' => 'nyu001@nyu.edu',
+        ),
+        'user2:user2pass' => array(
+            'uid' => 'nyu002',
+            'givenName' => 'Givenname2',
+            'surname' => 'Surname2',
+            'eduPersonPrincipleName' => 'nyu002@nyu.edu',
+        ),
+	    'user3:user3pass' => array(
+            'uid' => 'nyu003',
+            'givenName' => 'Givenname3',
+            'surname' => 'Surname3',
+            'eduPersonPrincipleName' => 'nyu003@nyu.edu',
+        ),
+	    'user4:user4pass' => array(
+            'uid' => 'nyu004',
+            'givenName' => 'Givenname4',
+            'surname' => 'Surname4',
+            'eduPersonPrincipleName' => 'nyu004@nyu.edu',
+        ),
+	    'user5:user5pass' => array(
+            'uid' => 'nyu005',
+            'givenName' => 'Givenname5',
+            'surname' => 'Surname5',
+            'eduPersonPrincipleName' => 'nyu005@nyu.edu',
+        ),
+	    'user6:user6pass' => array(
+            'uid' => 'nyu006',
+            'givenName' => 'Givenname6',
+            'surname' => 'Surname6',
+            'eduPersonPrincipleName' => 'nyu006@nyu.edu',
+        ),
+    ),
+);
