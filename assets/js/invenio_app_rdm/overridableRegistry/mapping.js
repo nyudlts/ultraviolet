0adf2e6b53a5ba45fb5481f31436c374889086ed
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-10-10 13:24:41 -0700

    Override Fileuploader component

--- assets/js/invenio_app_rdm/overridableRegistry/mapping.js
+++ assets/js/invenio_app_rdm/overridableRegistry/mapping.js
@@ -1,3 +1,9 @@
+// import { NewVersionButtonContainerOverride } from "./components/NewVersionButton"
+import { FileUploader } from "./components/FileUploader"
+
 export const overriddenComponents = {
-    // "InvenioAppRdm.Deposit.CommunityHeader.layout": () => null,
+  // use this to test and hide the communities section:
+  // "InvenioAppRdm.Deposit.CommunityHeader.layout": () => null,
+  // react-invenio-deposit v1.2.0
+  "InvenioAppRdm.Deposit.FileUploader.layout": FileUploader,
 };
