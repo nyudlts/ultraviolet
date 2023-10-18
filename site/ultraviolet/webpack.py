29304147e2ca31a7fd56d0dff763791b3b0db940
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-10-17 17:12:13 -0700

    Add message component to webpack

--- site/ultraviolet/webpack.py
+++ site/ultraviolet/webpack.py
@@ -10,6 +10,7 @@ theme = WebpackThemeBundle(
         "semantic-ui": dict(
             entry={
                 # Add your webpack entrypoints
+                "file-upload-limit-message": "./js/ultraviolet/FileUploadLimitMessage.js"
             },
         ),
     },
