ff93d2808143d4875a5faf3ecb4b80e72c0f2b06
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-11-02 14:22:52 -0700

    Add translations and email link

--- site/ultraviolet/assets/semantic-ui/js/ultraviolet/FileUploadLimitMessage.js
+++ site/ultraviolet/assets/semantic-ui/js/ultraviolet/FileUploadLimitMessage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Message, Icon } from "semantic-ui-react";
+import { i18next } from "@translations/invenio_app_rdm/i18next";
 
 const rootContainer = document.getElementById("div.ui.visible.warning.message.parent").parentNode;
 
@@ -8,11 +9,10 @@ ReactDOM.render(
   <Message visible warning>
     <p>
       <Icon name="warning sign" />
-      {i18next.t(
-        "File addition, removal or modification are not allowed after you have published your upload."
-      )}
+      {i18next.t("Contact the NYU Libraries UltraViolet service team at ")}
+      <a href="mailto:email@example.com">uv@nyu.edu</a>
+      {i18next.t(" if you have more than 100 files or 50GB.")}
     </p>
-  </Message>
-  ,
+  </Message>,
   rootContainer
 );
