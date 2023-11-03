797ee506e6a3ab1c08673984fa7e47b2675a234e
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-11-03 13:30:16 -0700

    Add back original warning message
    - lost from the render override.

--- site/ultraviolet/assets/semantic-ui/js/ultraviolet/FileUploadLimitMessage.js
+++ site/ultraviolet/assets/semantic-ui/js/ultraviolet/FileUploadLimitMessage.js
@@ -3,16 +3,25 @@ import ReactDOM from "react-dom";
 import { Message, Icon } from "semantic-ui-react";
 import { i18next } from "@translations/invenio_app_rdm/i18next";
 
-const rootContainer = document.getElementById("div.ui.visible.warning.message.parent").parentNode;
+const rootContainer = document.querySelector("div.ui.visible.warning.message").parentNode;
 
 ReactDOM.render(
-  <Message visible warning>
-    <p>
-      <Icon name="warning sign" />
-      {i18next.t("Contact the NYU Libraries UltraViolet service team at ")}
-      <a href="mailto:email@example.com">uv@nyu.edu</a>
-      {i18next.t(" if you have more than 100 files or 50GB.")}
-    </p>
-  </Message>,
+  <>
+    <Message visible warning>
+      <p>
+        <Icon name="warning sign" />
+        {i18next.t("File addition, removal or modification are not allowed after you have published your upload.")}
+      </p>
+    </Message>
+    <Message visible warning>
+      <p>
+        <Icon name="warning sign" />
+        {i18next.t("Contact the NYU Libraries UltraViolet service team at ")}
+        <a href="mailto:email@example.com">uv@nyu.edu</a>
+        {i18next.t(" if you have more than 100 files or 50GB.")}
+      </p>
+    </Message>
+  </>
+  ,
   rootContainer
 );
