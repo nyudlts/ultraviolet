bbfb8a7133d284ed1190c41efea827203ded1db4
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-11-08 10:16:51 -0800

    Add waitForElement helper function

---
+++ site/ultraviolet/assets/semantic-ui/js/ultraviolet/waitForElement.js
@@ -0,0 +1,24 @@
+/**
+ * waits for the dom to load and places an observer on the body
+ * to wait for more changes on the DOM to look up that element
+ * should be called within a DOMContentLoaded
+ * @param {string} selector 
+ * @param {function} callback 
+ */
+// https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists
+export async function waitForElement(selector) {
+  return new Promise(resolve => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector))
+    }    
+    const observer = new MutationObserver(mutations => {
+      if (document.querySelector(selector)) {
+        observer.disconnect()
+        resolve(document.querySelector(selector))
+      }
+    })    
+    observer.observe(document.body, { childList: true, subtree: true })
+  })
+}
+
+export default waitForElement;
