e733e91c3b0c97eddcf260d0a5265c6000aba3ac
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-11-08 10:17:41 -0800

    Use waitForElement, remove translation usage

--- site/ultraviolet/assets/semantic-ui/js/ultraviolet/FileUploadLimitMessage.js
+++ site/ultraviolet/assets/semantic-ui/js/ultraviolet/FileUploadLimitMessage.js
@@ -1,27 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Message, Icon } from "semantic-ui-react";
-import { i18next } from "@translations/invenio_app_rdm/i18next";
+import waitForElement from "./waitForElement";
 
-const rootContainer = document.querySelector("div.ui.visible.warning.message").parentNode;
+document.addEventListener('DOMContentLoaded', async () => {
 
-ReactDOM.render(
-  <>
-    <Message visible warning key="file-upload-message-warning-1">
-      <p>
-        <Icon name="warning sign" />
-        {i18next.t("File addition, removal or modification are not allowed after you have published your upload.")}
-      </p>
-    </Message>
-    <Message visible warning key="file-upload-message-warning-2">
-      <p>
-        <Icon name="warning sign" />
-        {i18next.t("Contact the NYU Libraries UltraViolet service team at ")}
-        <a href="mailto:email@example.com">uv@nyu.edu</a>
-        {i18next.t(" if you have more than 100 files or 50GB.")}
-      </p>
-    </Message>
-  </>
-  ,
-  rootContainer
-);
+  const fileUploadTargetExists = await waitForElement('.file-upload-note > div')
+  if (fileUploadTargetExists) {
+
+    const rootContainer = document.querySelector(".file-upload-note > div");
+    // FIXME: causes a double render error for now
+    ReactDOM.render(
+      <>
+        <Message visible warning key="file-upload-message-warning-1">
+          <p>
+            <Icon name="warning sign" />
+            File addition, removal or modification are not allowed after you have published your upload.
+          </p>
+        </Message>
+        <Message visible warning key="file-upload-message-warning-2">
+          <p>
+            <Icon name="warning sign" />
+            Contact the NYU Libraries UltraViolet service team at <a href="mailto:uv@nyu.com">uv@nyu.edu</a> if you have more than 100 files or 50GB.
+          </p>
+        </Message>
+      </>
+      ,
+      rootContainer
+    );
+  }
+});
