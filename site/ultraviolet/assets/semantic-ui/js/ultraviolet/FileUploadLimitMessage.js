69a3a157b3b6ad76001048ba2742ae74b6631765
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-11-09 11:09:46 -0800

    Remove react, use vanilla JS for err msg

--- site/ultraviolet/assets/semantic-ui/js/ultraviolet/FileUploadLimitMessage.js
+++ site/ultraviolet/assets/semantic-ui/js/ultraviolet/FileUploadLimitMessage.js
@@ -1,32 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Message, Icon } from "semantic-ui-react";
-import waitForElement from "./waitForElement";
+import { waitForElement } from "./waitForElement";
 
 document.addEventListener('DOMContentLoaded', async () => {
 
-  const fileUploadTargetExists = await waitForElement('.file-upload-note > div')
-  if (fileUploadTargetExists) {
-
-    const rootContainer = document.querySelector(".file-upload-note > div");
-    // FIXME: causes a double render error for now
-    ReactDOM.render(
-      <>
-        <Message visible warning key="file-upload-message-warning-1">
-          <p>
-            <Icon name="warning sign" />
-            File addition, removal or modification are not allowed after you have published your upload.
-          </p>
-        </Message>
-        <Message visible warning key="file-upload-message-warning-2">
-          <p>
-            <Icon name="warning sign" />
-            Contact the NYU Libraries UltraViolet service team at <a href="mailto:uv@nyu.com">uv@nyu.edu</a> if you have more than 100 files or 50GB.
-          </p>
-        </Message>
-      </>
-      ,
-      rootContainer
-    );
+  const fileUploadWarning = await waitForElement("div.ui.visible.warning.message")
+  if (fileUploadWarning) {
+    let secondWarning = fileUploadWarning.cloneNode(true) // true for deepclone
+    // using replace to preserve the icon
+    secondWarning.innerHTML = secondWarning.innerHTML.replace("File addition, removal or modification are not allowed after you have published your upload.", "Contact the NYU Libraries UltraViolet service team at <a href='mailto:uv@nyu.edu'>uv@nyu.edu</a> if you have more than 100 files or 50GB.")
+    // satisfying react's need for unique IDs
+    secondWarning.id = fileUploadWarning.id + "1"
+    fileUploadWarning.parentNode.append(secondWarning)
   }
 });
