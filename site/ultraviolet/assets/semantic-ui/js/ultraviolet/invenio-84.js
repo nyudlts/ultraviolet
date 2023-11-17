b4015796aeeb14d12cc83abae8a3c015509cc3fc
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-11-17 09:06:34 -0800

    Separate changes to deposit into modules by ticket

---
+++ site/ultraviolet/assets/semantic-ui/js/ultraviolet/invenio-84.js
@@ -0,0 +1,17 @@
+import { waitForElement } from "./helpers"
+
+/**
+ * Adds a warning message to the file upload section of the deposit form
+ * https://jira.nyu.edu/browse/INVENIO-84
+ */
+export async function invenio84() {
+  const fileUploadWarning = await waitForElement(".file-upload-note div.ui.visible.warning.message")
+  if (fileUploadWarning) {
+    let secondWarning = document.createElement("div")
+    secondWarning.classList.add("ui", "visible",  "warning", "message")
+    secondWarning.setAttribute("id", "invenio-84-file-limits-msg")
+    const message = `Contact the NYU Libraries UltraViolet service team at <a href="mailto:uv@nyu.edu">uv@nyu.edu</a> if you have more than 100 files or 50GB.`
+    secondWarning.innerHTML = `<p><i aria-hidden="true" class="warning sign icon"></i>${message}</p>`
+    fileUploadWarning.parentNode.append(secondWarning)
+  }
+}
--- site/ultraviolet/assets/semantic-ui/js/ultraviolet/ultraviolet.deposit.js
+++ site/ultraviolet/assets/semantic-ui/js/ultraviolet/ultraviolet.deposit.js
@@ -1,14 +1,4 @@
-import { waitForElement } from "./waitForElement";
+// acts as a root for javascript that needs to run in the deposit form
+import { invenio84 } from "./invenio-84"
 
-document.addEventListener('DOMContentLoaded', async () => {
-
-  const fileUploadWarning = await waitForElement("div.ui.visible.warning.message")
-  if (fileUploadWarning) {
-    let secondWarning = fileUploadWarning.cloneNode(true) // true for deepclone
-    // using replace to preserve the icon
-    secondWarning.innerHTML = secondWarning.innerHTML.replace("File addition, removal or modification are not allowed after you have published your upload.", "Contact the NYU Libraries UltraViolet service team at <a href='mailto:uv@nyu.edu'>uv@nyu.edu</a> if you have more than 100 files or 50GB.")
-    // satisfying react's need for unique IDs
-    secondWarning.id = fileUploadWarning.id + "1"
-    fileUploadWarning.parentNode.append(secondWarning)
-  }
-});
+invenio84()
