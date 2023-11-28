1b56bf81bb8721da0d53f0e363468124c6b0fb95
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-11-28 11:52:23 -0800

    Change selector to wait for parent

--- site/ultraviolet/assets/semantic-ui/js/ultraviolet/invenio-84.js
+++ site/ultraviolet/assets/semantic-ui/js/ultraviolet/invenio-84.js
@@ -5,13 +5,13 @@ import { waitForElement } from "./helpers"
  * https://jira.nyu.edu/browse/INVENIO-84
  */
 export async function invenio84() {
-  const fileUploadWarning = await waitForElement(".file-upload-note div.ui.visible.warning.message")
-  if (fileUploadWarning) {
+  const fileUploadWarningContainer = await waitForElement(".file-upload-note div.column")
+  if (fileUploadWarningContainer) {
     let secondWarning = document.createElement("div")
     secondWarning.classList.add("ui", "visible",  "warning", "message")
     secondWarning.setAttribute("id", "invenio-84-file-limits-msg")
     const message = `Contact the NYU Libraries UltraViolet service team at <a href="mailto:uv@nyu.edu">uv@nyu.edu</a> if you have more than 100 files or 50GB.`
     secondWarning.innerHTML = `<p><i aria-hidden="true" class="warning sign icon"></i>${message}</p>`
-    fileUploadWarning.parentNode.append(secondWarning)
+    fileUploadWarningContainer.append(secondWarning)
   }
 }
