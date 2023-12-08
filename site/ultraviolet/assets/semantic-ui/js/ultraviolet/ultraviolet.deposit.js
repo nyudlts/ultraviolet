798ca52573017900a4587b8b832bf06558bcd56e
Jonathan Ahrens <18541228+teikmeout@users.noreply.github.com>
2023-12-08 10:26:44 -0800

    Add deposit label changes to deposit form

--- site/ultraviolet/assets/semantic-ui/js/ultraviolet/ultraviolet.deposit.js
+++ site/ultraviolet/assets/semantic-ui/js/ultraviolet/ultraviolet.deposit.js
@@ -1,4 +1,6 @@
 // acts as a root for javascript that needs to run in the deposit form
 import { uvFileLimitsMessage } from "./uv-file-limits-msg"
+import { uvDepositDatesLabel } from "./uv-deposit-dates-label"
 
 uvFileLimitsMessage()
+uvDepositDatesLabel()
