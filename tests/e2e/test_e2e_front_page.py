0340576af38141d8c7d6993c25d0243109d40d88
zuchuandatou <zacliel56@gmail.com>
2024-08-21 15:40:59 -0400

    Add step to upload screenshot

--- tests/e2e/test_e2e_front_page.py
+++ tests/e2e/test_e2e_front_page.py
@@ -9,7 +9,8 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 """E2E test of the front page."""
 
-from flask import url_for
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import multiprocessing
 
@@ -17,8 +18,29 @@ import multiprocessing
     https://github.com/pytest-dev/pytest-flask/issues/104 """
 multiprocessing.set_start_method("fork")
 
+def create_chrome_driver():
+    chrome_options = Options()
+    chrome_options.add_argument("disable-blink-features=AutomationControlled")
+    chrome_options.add_argument('--headless=new')
+    chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-software-rasterizer")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
+    chrome_options.page_load_strategy = 'none'
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_argument('--ignore-ssl-errors=yes')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+    
+    return driver
 
-def test_frontpage(live_server, browser):
+
+def test_frontpage(live_server):
     """Test retrieval of front page."""
-    browser.get(url_for("invenio_app_rdm.index", _external=True))
+    browser = create_chrome_driver()
+    browser.get("https://127.0.0.1:5000/")
     assert "Search UltraViolet" == browser.find_element(By.TAG_NAME, "h1").text
