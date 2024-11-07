493b553ca9f66a1e5097102d86d62e820b9c63c9
Brian Kelly <polymonic@gmail.com>
2024-11-07 12:58:26 -0600

    Leaflet map with WMS visuals and clicking for feature attributes

---
+++ site/ultraviolet/assets/semantic-ui/js/ultraviolet/geoserver.js
@@ -0,0 +1,135 @@
+import L from "leaflet"
+import "leaflet/dist/leaflet.css";
+
+const populateAttributeTable = (data) => {
+    const attributesElement = document.getElementById("attributes");
+    attributesElement.innerHTML = '';
+
+    Object.keys(data.properties).sort().forEach(property => {
+        const nameTd = document.createElement('td');
+        nameTd.textContent = property
+        const typeTd = document.createElement("td")
+        typeTd.textContent = data.properties[property]
+
+        const tr = document.createElement("tr")
+        tr.appendChild(nameTd)
+        tr.appendChild(typeTd)
+        attributesElement.appendChild(tr)
+    })
+
+}
+
+const describeFeatureType = (wfsUrl, layerNames) => {
+    const attributesElement = document.getElementById("attributes");
+
+    const formData = new FormData();
+    formData.append("url", wfsUrl);
+    formData.append("layers", layerNames);
+
+    fetch("/geoserver/describe_feature_type", {
+        method: "POST", body: formData
+    })
+        .then(response => response.json())
+        .then(data => {
+            const attributes = data.featureTypes[0].properties
+            attributes.forEach(attribute => {
+                const nameTd = document.createElement('td');
+                nameTd.textContent = attribute.name
+                const typeTd = document.createElement("td")
+                typeTd.textContent = attribute.localType
+
+                const tr = document.createElement("tr")
+                tr.appendChild(nameTd)
+                tr.appendChild(typeTd)
+                attributesElement.appendChild(tr)
+            })
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+};
+
+const addInspection = (map, url, layerId) => {
+    map.on("click", async (e) => {
+        const attributesElement = document.getElementById("attributes");
+        attributesElement.innerHTML = '<tr><td colspan="2">Loading...</td>';
+
+        const wmsoptions = {
+            url: url,
+            layers: layerId,
+            bbox: map.getBounds().toBBoxString(),
+            width: Math.round(document.getElementById("map").clientWidth),
+            height: Math.round(document.getElementById("map").clientHeight),
+            query_layers: layerId,
+            x: Math.round(e.containerPoint.x),
+            y: Math.round(e.containerPoint.y),
+        };
+
+        try {
+            const response = await fetch("/geoserver/get_feature_info", {
+                method: "POST", headers: {
+                    "Content-Type": "application/json",
+                }, body: JSON.stringify(wmsoptions),
+            });
+
+            if (!response.ok) throw new Error("Network response was not ok.");
+
+            const response_data = await response.json();
+
+            if (response_data.hasOwnProperty("error") || response_data.hasOwnProperty('exceptions') || response_data.features.length === 0) {
+                const attributesElement = document.getElementById("attributes");
+                attributesElement.innerHTML = '<tr><td colspan="2">No feature found</td>';
+
+                return;
+            }
+            const data = response_data.features[0];
+            // if (!data.isHTML) overlayLayer(map, data, layer);
+
+            populateAttributeTable(data);
+        } catch (error) {
+            console.error("Fetch error: ", error);
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const attributesElement = document.getElementById("attributes");
+    const wfsUrl = attributesElement.getAttribute("data-wfs-url");
+    const layerNames = attributesElement.getAttribute("data-layer-names");
+
+
+    const mapElement = document.getElementById("map");
+    const baseUrl = mapElement.getAttribute("data-base-url")
+    const layerName = mapElement.getAttribute("data-layer-name")
+    const bounds = mapElement.getAttribute("data-bounds")
+
+    const map = L.map('map').setView([0, 0], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    const wmsLayer = L.tileLayer.wms(baseUrl, {
+        layers: layerName, format: 'image/png', transparent: true,
+    });
+
+    wmsLayer.addTo(map);
+    wmsLayer.setOpacity(0.75);
+
+    const regex = /ENVELOPE\(([-\d.]+), ([-\d.]+), ([-\d.]+), ([-\d.]+)\)/;
+    const match = bounds.match(regex);
+
+    if (match) {
+        const minLon = parseFloat(match[1]);
+        const maxLon = parseFloat(match[2]);
+        const minLat = parseFloat(match[3]);
+        const maxLat = parseFloat(match[4]);
+
+        const bounds = [[minLat, minLon], [maxLat, maxLon]];
+
+        map.fitBounds(bounds);
+    }
+
+    addInspection(map, wfsUrl, layerNames);
+    describeFeatureType(wfsUrl, layerNames, attributesElement);
+});
---
+++ tests/ui/test_map_view.py
@@ -0,0 +1,22 @@
+from invenio_access.permissions import system_identity
+
+
+def test_map_view(app, service, minimal_record, client_with_login):
+    data = minimal_record.copy()
+    data["metadata"]["title"] = "Geospatial Data"
+    data["custom_fields"] = {
+        "geoserver:wms_url": "https://maps-public.geo.nyu.edu/geoserver/sdr/wms",
+        "geoserver:layer_name": "sdr:nyu_2451_34156",
+        "geoserver:bounds": "ENVELOPE(-74.2556640887564, -73.700009054899, 40.9157739339836, 40.4960925239255)"
+    }
+
+    draft = service.create(system_identity, data)
+    record = service.publish(system_identity, draft.id)
+
+    record_view = client_with_login.get("/records/" + record['id']).data
+    html = record_view.decode("utf-8")
+
+    assert "Geospatial Data" in html
+    assert "data-base-url=\"https://maps-public.geo.nyu.edu/geoserver/sdr/wms\"" in html
+    assert "data-layer-name=\"sdr:nyu_2451_34156\"" in html
+    assert "data-bounds=\"ENVELOPE(-74.2556640887564, -73.700009054899, 40.9157739339836, 40.4960925239255)\"" in html
