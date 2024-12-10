eca1901f2dc8daa8cf7f638d563955d0d209f710
Brian Kelly <polymonic@gmail.com>
2024-12-10 15:06:42 -0600

    Switch to booleans and config vars for GeoServer metadata and URLs

--- tests/ui/test_map_view.py
+++ tests/ui/test_map_view.py
@@ -1,11 +1,12 @@
 from invenio_access.permissions import system_identity
 
 
-def test_map_view(app, service, minimal_record, client_with_login):
+def test_public_map_view(app, service, minimal_record, client_with_login):
     data = minimal_record.copy()
     data["metadata"]["title"] = "Geospatial Data"
     data["custom_fields"] = {
-        "geoserver:wms_url": "https://maps-public.geo.nyu.edu/geoserver/sdr/wms",
+        "geoserver:has_wms_layer": True,
+        "geoserver:has_wfs_layer": True,
         "geoserver:layer_name": "sdr:nyu_2451_34156",
         "geoserver:bounds": "ENVELOPE(-74.2556640887564, -73.700009054899, 40.9157739339836, 40.4960925239255)"
     }
@@ -18,5 +19,27 @@ def test_map_view(app, service, minimal_record, client_with_login):
 
     assert "Geospatial Data" in html
     assert "data-wms-url=\"https://maps-public.geo.nyu.edu/geoserver/sdr/wms\"" in html
+    assert "data-layer-name=\"sdr:nyu_2451_34156\"" in html
+    assert "data-bounds=\"ENVELOPE(-74.2556640887564, -73.700009054899, 40.9157739339836, 40.4960925239255)\"" in html
+
+def test_restricted_map_view(app, service, minimal_record, client_with_login):
+    data = minimal_record.copy()
+    data["access"]["files"] = "restricted"
+    data["metadata"]["title"] = "Geospatial Data"
+    data["custom_fields"] = {
+        "geoserver:has_wms_layer": True,
+        "geoserver:has_wfs_layer": True,
+        "geoserver:layer_name": "sdr:nyu_2451_34156",
+        "geoserver:bounds": "ENVELOPE(-74.2556640887564, -73.700009054899, 40.9157739339836, 40.4960925239255)"
+    }
+
+    draft = service.create(system_identity, data)
+    record = service.publish(system_identity, draft.id)
+
+    record_view = client_with_login.get("/records/" + record['id']).data
+    html = record_view.decode("utf-8")
+
+    assert "Geospatial Data" in html
+    assert "data-wms-url=\"https://maps-restricted.geo.nyu.edu/geoserver/sdr/wms\"" in html
     assert "data-layer-name=\"sdr:nyu_2451_34156\"" in html
     assert "data-bounds=\"ENVELOPE(-74.2556640887564, -73.700009054899, 40.9157739339836, 40.4960925239255)\"" in html
