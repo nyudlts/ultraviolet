3a6d084b6312d95e91f6a4ac0c174c79d011170d
Brian Kelly <polymonic@gmail.com>
2024-12-11 14:05:44 -0600

    Fix tests after rebase

--- tests/ui/test_map_view.py
+++ tests/ui/test_map_view.py
@@ -1,8 +1,8 @@
 from invenio_access.permissions import system_identity
 
 
-def test_public_map_view_when_anonymous(service, geospatial_record, client):
-    published_record = publish_draft(geospatial_record, service)
+def test_public_map_view_when_anonymous(services, geospatial_record, client):
+    published_record = publish_draft(geospatial_record, services)
 
     html = client.get("/records/" + published_record['id']).data.decode("utf-8")
 
@@ -15,8 +15,8 @@ def test_public_map_view_when_anonymous(service, geospatial_record, client):
     assert "data-bounds=\"ENVELOPE(-74.2556640887564, -73.700009054899, 40.9157739339836, 40.4960925239255)\"" in html
 
 
-def test_restricted_map_view_when_logged_in(service, restricted_geospatial_record, client_with_login):
-    published_record = publish_draft(restricted_geospatial_record, service)
+def test_restricted_map_view_when_logged_in(services, restricted_geospatial_record, client_with_login):
+    published_record = publish_draft(restricted_geospatial_record, services)
 
     html = client_with_login.get("/records/" + published_record['id']).data.decode("utf-8")
 
@@ -29,8 +29,8 @@ def test_restricted_map_view_when_logged_in(service, restricted_geospatial_recor
     assert "data-bounds=\"ENVELOPE(-74.2556640887564, -73.700009054899, 40.9157739339836, 40.4960925239255)\"" in html
 
 
-def test_restricted_map_view_when_anonymous(service, restricted_geospatial_record, client):
-    published_record = publish_draft(restricted_geospatial_record, service)
+def test_restricted_map_view_when_anonymous(services, restricted_geospatial_record, client):
+    published_record = publish_draft(restricted_geospatial_record, services)
 
     html = client.get("/records/" + published_record['id']).data.decode("utf-8")
 
@@ -43,8 +43,8 @@ def test_restricted_map_view_when_anonymous(service, restricted_geospatial_recor
     assert "data-layer-name=\"sdr:nyu_2451_34156\"" not in html
 
 
-def publish_draft(data, service):
-    draft = service.create(system_identity, data)
-    record = service.publish(system_identity, draft.id)
+def publish_draft(data, services):
+    draft = services.create(system_identity, data)
+    record = services.publish(system_identity, draft.id)
 
     return record
