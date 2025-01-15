8f39ea0fcb1a7265b76de21253f476f0824ed744
Brian Kelly <polymonic@gmail.com>
2025-01-15 12:26:21 -0600

    Rename title and program fields to layer and bounds

--- assets/templates/custom_fields/Experiments.js
+++ assets/templates/custom_fields/Experiments.js
@@ -3,16 +3,16 @@ import React, {Component} from "react";
 import {Input} from "react-invenio-forms";
 
 const newExperiment = {
-  title: "",
-  program: "",
+  layer: "",
+  bounds: "",
 };
 
 export class Experiments extends Component {
   render() {
     const {
       fieldPath, // injected by the custom field loader via the `field` config property
-      title,
-      program,
+      layer,
+      bounds,
     } = this.props;
 
     const fieldPathPrefix = `${fieldPath}`;
@@ -20,16 +20,16 @@ export class Experiments extends Component {
     return (
       <>
         <Input
-          fieldPath={`${fieldPathPrefix}.title`}
-          label={title.label}
-          placeholder={title.placeholder}
-          description={title.description}
+          fieldPath={`${fieldPathPrefix}.layer`}
+          label={layer.label}
+          placeholder={layer.placeholder}
+          description={layer.description}
         ></Input>
         <Input
-          fieldPath={`${fieldPathPrefix}.program`}
-          label={program.label}
-          placeholder={program.placeholder}
-          description={program.description}
+          fieldPath={`${fieldPathPrefix}.bounds`}
+          label={bounds.label}
+          placeholder={bounds.placeholder}
+          description={bounds.description}
         ></Input>
       </>
     );
