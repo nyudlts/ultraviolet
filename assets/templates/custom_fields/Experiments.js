f91ed69bfb6f68ed71a2a414fa02d7bab95508e6
Brian Kelly <polymonic@gmail.com>
2025-01-15 11:35:57 -0600

    Switch from multiple sets of fields to single set of fields

--- assets/templates/custom_fields/Experiments.js
+++ assets/templates/custom_fields/Experiments.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
-import { Input, Array } from "react-invenio-forms";
-import { Grid, Form, Button, Icon } from "semantic-ui-react";
+import {Input, Array} from "react-invenio-forms";
+import {Grid, Form, Button, Icon} from "semantic-ui-react";
 
 const newExperiment = {
   title: "",
@@ -19,53 +19,29 @@ export class Experiments extends Component {
       description,
       label,
     } = this.props;
+
+    const fieldPathPrefix = `${fieldPath}`;
+
     return (
-      <Array
-        fieldPath={fieldPath}
-        label={label}
-        icon={icon}
-        addButtonLabel={addButtonLabel}
-        defaultNewValue={newExperiment}
-        description={description}
-      >
-        {({ arrayHelpers, indexPath }) => {
-          const fieldPathPrefix = `${fieldPath}.${indexPath}`;
-          return (
-            <Grid>
-              <Grid.Column width="7">
-                <Input
-                  fieldPath={`${fieldPathPrefix}.title`}
-                  label={title.label}
-                  placeholder={title.placeholder}
-                  description={title.description}
-                ></Input>
-              </Grid.Column>
-              <Grid.Column width="8">
-                <Input
-                  fieldPath={`${fieldPathPrefix}.program`}
-                  label={program.label}
-                  icon={"building"}
-                  placeholder={program.placeholder}
-                  description={program.description}
-                ></Input>
-              </Grid.Column>
-              <Grid.Column width="1">
-                <Form.Field style={{ marginTop: "1.75rem", float: "right" }}>
-                  <Button
-                    aria-label={"Remove field"}
-                    className="close-btn"
-                    icon
-                    onClick={() => arrayHelpers.remove(indexPath)}
-                    type="button"
-                  >
-                    <Icon name="close" />
-                  </Button>
-                </Form.Field>
-              </Grid.Column>
-            </Grid>
-          );
-        }}
-      </Array>
+      <Grid>
+        <Grid.Column width="7">
+          <Input
+            fieldPath={`${fieldPathPrefix}.title`}
+            label={title.label}
+            placeholder={title.placeholder}
+            description={title.description}
+          ></Input>
+        </Grid.Column>
+        <Grid.Column width="8">
+          <Input
+            fieldPath={`${fieldPathPrefix}.program`}
+            label={program.label}
+            icon={"building"}
+            placeholder={program.placeholder}
+            description={program.description}
+          ></Input>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
