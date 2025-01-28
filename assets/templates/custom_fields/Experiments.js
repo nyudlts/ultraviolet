f13f3a0ea4855d73a2f8e5eaf2b3414df587793f
Brian Kelly <polymonic@gmail.com>
2025-01-28 08:28:24 -0600

    Basic map preview while depositing

--- assets/templates/custom_fields/Experiments.js
+++ assets/templates/custom_fields/Experiments.js
@@ -1,12 +1,12 @@
-import React, {Component, useEffect, useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 import {BooleanCheckbox, Input} from "react-invenio-forms";
 import {Grid, GridColumn} from 'semantic-ui-react'
+import {useFormikContext} from 'formik';
 import L from 'leaflet';
-
 import 'leaflet/dist/leaflet.css';
 
-const Map = ({center = [0, 0], zoom = 1}) => {
+const Map = ({layerName = "", boundingBox = "", center = [0, 0], zoom = 1}) => {
   const mapRef = useRef(null);
 
   useEffect(() => {
@@ -19,56 +19,122 @@ const Map = ({center = [0, 0], zoom = 1}) => {
       retina: "@2x",
     }).addTo(map);
 
+    if (layerName != "") {
+      const wmsLayer = L.tileLayer.wms("https://maps-public.geo.nyu.edu/geoserver/sdr/wms", {
+        layers: layerName,
+        format: 'image/png',
+        transparent: true,
+        opacity: 0.75
+      });
+
+      wmsLayer.addTo(map);
+      wmsLayer.setOpacity(0.75);
+    }
+
+    const match = boundingBox.match(/ENVELOPE\(([-\d.]+), ([-\d.]+), ([-\d.]+), ([-\d.]+)\)/);
+
+    if (match) {
+      let [_, minLon, maxLon, minLat, maxLat] = match;
+
+      const bounds = [[minLat, minLon], [maxLat, maxLon]];
+
+      map.fitBounds(bounds);
+    }
+
     return () => map.remove();
-  }, [center, zoom]);
+  }, [center, zoom, layerName]);
 
   return (
     <div ref={mapRef} id="map" style={{height: '440px', width: '100%'}}/>
   );
 };
 
-export class Experiments extends Component {
-  render() {
-    const {
-      fieldPath, // injected by the custom field loader via the `field` config property
-      layer,
-      has_wms,
-      has_wfs,
-      bounds,
-    } = this.props;
+export const Experiments = props => {
+  const {
+    fieldPath, // injected by the custom field loader via the `field` config property
+    layer,
+    has_wms,
+    has_wfs,
+    bounds,
+  } = props;
 
-    const fieldPathPrefix = `${fieldPath}`;
+  const [layerName, setLayerName] = useState("")
+  const [hasWms, setHasWms] = useState(false)
+  const [hasWfs, setHasWfs] = useState(false)
+  const [boundingBox, setBoundingBox] = useState("")
 
-    return (
-      <>
-        <Input
-          fieldPath={`${fieldPathPrefix}.layer`}
-          label={layer.label}
-          placeholder={layer.placeholder}
-          description={layer.description}
-        ></Input>
-        <BooleanCheckbox
-          fieldPath={`${fieldPathPrefix}.has_wms`}
-          label={has_wms.label}
-          description={has_wms.description}
-        ></BooleanCheckbox>
-        <BooleanCheckbox
-          fieldPath={`${fieldPathPrefix}.has_wfs`}
-          label={has_wfs.label}
-          description={has_wfs.description}
-        ></BooleanCheckbox>
-        <Input
-          fieldPath={`${fieldPathPrefix}.bounds`}
-          label={bounds.label}
-          placeholder={bounds.placeholder}
-          description={bounds.description}
-        ></Input>
+  const fieldPathPrefix = `${fieldPath}`;
+
+  const {values} = useFormikContext();
+
+  useEffect(() => {
+    console.log("Field value changed:", values);
+
+    if (values.custom_fields && values.custom_fields.experiments) {
+      if (values.custom_fields.experiments.layer) {
+        setLayerName(values.custom_fields.experiments.layer)
+      } else {
+        setLayerName("")
+      }
+
+      if (values.custom_fields.experiments.has_wms) {
+        setHasWms(true)
+      } else {
+        setHasWms(false)
+      }
+
+      if (values.custom_fields.experiments.has_wfs) {
+        setHasWfs(true)
+      } else {
+        setHasWfs(false)
+      }
+
+      if (values.custom_fields.experiments.bounds) {
+        setBoundingBox(values.custom_fields.experiments.bounds)
+      } else {
+        setBoundingBox("")
+      }
+    }
+  }, [values]);
+
+  return (
+    <>
+      <Input
+        fieldPath={`${fieldPathPrefix}.layer`}
+        label={layer.label}
+        placeholder={layer.placeholder}
+        description={layer.description}
+      ></Input>
+      <BooleanCheckbox
+        fieldPath={`${fieldPathPrefix}.has_wms`}
+        label={has_wms.label}
+        description={has_wms.description}
+      ></BooleanCheckbox>
+      {layerName && hasWms && (
         <Grid>
           <GridColumn>
-            <Map></Map>
+            <Map layerName={layerName} boundingBox={boundingBox}></Map>
           </GridColumn>
         </Grid>
-      </>
-    );
-  }
-}
+      )}
+      <BooleanCheckbox
+        fieldPath={`${fieldPathPrefix}.has_wfs`}
+        label={has_wfs.label}
+        description={has_wfs.description}
+      ></BooleanCheckbox>
+      {layerName && hasWfs && (
+        <Grid>
+          <GridColumn>
+            WFS Table Here (eventually)
+          </GridColumn>
+        </Grid>
+      )}
+      <Input
+        fieldPath={`${fieldPathPrefix}.bounds`}
+        label={bounds.label}
+        placeholder={bounds.placeholder}
+        description={bounds.description}
+      ></Input>
+    </>
+  );
+};
