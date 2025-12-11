bd4317c7085a5da03fd145c7a09ab1fbc0e0e5d7
Brian Kelly <brian@spilth.org>
2025-12-11 09:18:29 -0600

    Extract some fixtures for less repetition

--- tests/ui/test_permissions.py
+++ tests/ui/test_permissions.py
@@ -8,18 +8,16 @@
 # ultraviolet is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 """Tests for restricted file message on record page."""
-
+import pytest
 from invenio_access.permissions import system_identity
 from invenio_communities import current_communities
-from invenio_communities.communities.records.api import Community
 from invenio_rdm_records.proxies import current_rdm_records_service, current_record_communities_service
 from invenio_search import current_search_client
 
 
-def test_user_without_special_role_cannot_access_restricted_records(services, app, db, client_with_login):
-    service = current_rdm_records_service
-
-    data = {
+@pytest.fixture()
+def restricted_record():
+    return {
         "pids": {},
         "access": {
             "record": "restricted",
@@ -33,114 +31,50 @@ def test_user_without_special_role_cannot_access_restricted_records(services, ap
             "resource_type": {"id": "image-photo"},
             "creators": [{
                 "person_or_org": {
-                    "family_name": "Brown",
-                    "given_name": "Troy",
-                    "type": "personal"
-                }
-            }, {
-                "person_or_org": {
                     "name": "Troy Inc.",
                     "type": "organizational",
                 },
             }],
-            # because DATACITE_ENABLED is True, this field is required
             "publisher": "Acme Inc",
             "title": "A Romans story",
         }
     }
 
-    draft = service.create(system_identity, data)
-    record = service.publish(system_identity, draft.id)
+
+@pytest.fixture(scope="module")
+def communities_index():
+    if not current_search_client.indices.exists(index="communities-communities-v2.0.0"):
+        current_search_client.indices.create(index="communities-communities-v2.0.0")
+
+
+def test_user_without_special_role_cannot_access_restricted_records(services, app, db, client_with_login,
+                                                                    restricted_record):
+    draft = current_rdm_records_service.create(system_identity, restricted_record.copy())
+    record = current_rdm_records_service.publish(system_identity, draft.id)
 
     response = client_with_login.get("/api/records/" + record['id'], headers={
         'content-type': 'application/octet-stream',
         'accept': 'application/json'
     })
+
     assert 403 == response.status_code
 
 
-def test_anonymous_user_cannot_access_restricted_records(services, app, db, base_client):
-    service = current_rdm_records_service
-
-    data = {
-        "pids": {},
-        "access": {
-            "record": "restricted",
-            "files": "restricted",
-        },
-        "files": {
-            "enabled": False,
-        },
-        "metadata": {
-            "publication_date": "2020-06-01",
-            "resource_type": {"id": "image-photo"},
-            "creators": [{
-                "person_or_org": {
-                    "family_name": "Brown",
-                    "given_name": "Troy",
-                    "type": "personal"
-                }
-            }, {
-                "person_or_org": {
-                    "name": "Troy Inc.",
-                    "type": "organizational",
-                },
-            }],
-            "publication_date": "2020-06-01",
-            # because DATACITE_ENABLED is True, this field is required
-            "publisher": "Acme Inc",
-            "resource_type": {"id": "image-photo"},
-            "title": "A Romans story",
-        }
-    }
-
-    draft = service.create(system_identity, data)
-    record = service.publish(system_identity, draft.id)
+def test_anonymous_user_cannot_access_restricted_records(services, app, db, base_client, restricted_record):
+    draft = current_rdm_records_service.create(system_identity, restricted_record.copy())
+    record = current_rdm_records_service.publish(system_identity, draft.id)
 
     response = base_client.get("/api/records/" + record['id'], headers={
         'content-type': 'application/octet-stream',
         'accept': 'application/json'
     })
+
     assert 403 == response.status_code
 
 
-def test_user_with_nyu_role_can_access_restricted_records(services, app, client_with_nyu_login):
-    service = current_rdm_records_service
-
-    data = {
-        "pids": {},
-        "access": {
-            "record": "restricted",
-            "files": "restricted"
-        },
-        "files": {
-            "enabled": False,
-        },
-        "metadata": {
-            "publication_date": "2020-06-01",
-            "resource_type": {"id": "image-photo"},
-            "creators": [{
-                "person_or_org": {
-                    "family_name": "Brown",
-                    "given_name": "Troy",
-                    "type": "personal"
-                }
-            }, {
-                "person_or_org": {
-                    "name": "Troy Inc.",
-                    "type": "organizational",
-                },
-            }],
-            "publication_date": "2020-06-01",
-            # because DATACITE_ENABLED is True, this field is required
-            "publisher": "Acme Inc",
-            "resource_type": {"id": "image-photo"},
-            "title": "A Romans story",
-        }
-    }
-
-    draft = service.create(system_identity, data)
-    record = service.publish(system_identity, draft.id)
+def test_user_with_nyu_role_can_access_restricted_records(services, app, client_with_nyu_login, restricted_record):
+    draft = current_rdm_records_service.create(system_identity, restricted_record.copy())
+    record = current_rdm_records_service.publish(system_identity, draft.id)
 
     response = client_with_nyu_login.get("/api/records/" + record['id'], headers={
         'content-type': 'application/octet-stream',
@@ -150,15 +84,10 @@ def test_user_with_nyu_role_can_access_restricted_records(services, app, client_
     assert 200 == response.status_code
 
 
-# Specifically, I’d like to verify that when a record contains a restricted
-# file and is assigned to a community, anonymous users can not see it but users
-# who belong to that community’s reader group are able to view the file. Since
-# we have customized permission policies, we want to ensure they don't interfere
-# with the default community access rules.
-
-def test_user_without_community_access_cannot_see_restricted_records(services, app, db, base_client):
-    if not current_search_client.indices.exists(index="communities-communities-v2.0.0"):
-        current_search_client.indices.create(index="communities-communities-v2.0.0")
+def test_user_without_community_access_cannot_see_restricted_records(services, app, db, base_client, restricted_record,
+                                                                     communities_index):
+    draft = current_rdm_records_service.create(identity=system_identity, data=(restricted_record.copy()))
+    record = current_rdm_records_service.publish(system_identity, draft.id)
 
     community = current_communities.service.create(
         identity=system_identity,
@@ -174,40 +103,6 @@ def test_user_without_community_access_cannot_see_restricted_records(services, a
             },
         })
 
-    draft = current_rdm_records_service.create(
-        identity=system_identity,
-        data={
-            "pids": {},
-            "access": {
-                "record": "restricted",
-                "files": "restricted"
-            },
-            "files": {
-                "enabled": False,
-            },
-            "metadata": {
-                "publication_date": "2020-06-01",
-                "resource_type": {"id": "image-photo"},
-                "creators": [{
-                    "person_or_org": {
-                        "family_name": "Brown",
-                        "given_name": "Troy",
-                        "type": "personal"
-                    }
-                }, {
-                    "person_or_org": {
-                        "name": "Troy Inc.",
-                        "type": "organizational",
-                    },
-                }],
-                "publication_date": "2020-06-01",
-                "publisher": "Acme Inc",
-                "resource_type": {"id": "image-photo"},
-                "title": "A Romans story",
-            }
-        })
-    record = current_rdm_records_service.publish(system_identity, draft.id)
-
     current_record_communities_service.add(
         system_identity,
         record["id"],
@@ -222,9 +117,10 @@ def test_user_without_community_access_cannot_see_restricted_records(services, a
     assert 403 == response.status_code
 
 
-def test_user_with_community_access_can_see_restricted_records(services, app, db, client_with_basic_user, users):
-    if not current_search_client.indices.exists(index="communities-communities-v2.0.0"):
-        current_search_client.indices.create(index="communities-communities-v2.0.0")
+def test_user_with_community_access_can_see_restricted_records(services, app, db, client_with_basic_user, users,
+                                                               restricted_record, communities_index):
+    draft = current_rdm_records_service.create(identity=system_identity, data=(restricted_record.copy()))
+    record = current_rdm_records_service.publish(system_identity, draft.id)
 
     community = current_communities.service.create(
         identity=system_identity,
@@ -248,40 +144,6 @@ def test_user_with_community_access_can_see_restricted_records(services, app, db
             "role": "reader"
         }
     )
-
-    draft = current_rdm_records_service.create(
-        identity=system_identity,
-        data={
-            "pids": {},
-            "access": {
-                "record": "restricted",
-                "files": "restricted"
-            },
-            "files": {
-                "enabled": False,
-            },
-            "metadata": {
-                "publication_date": "2020-06-01",
-                "resource_type": {"id": "image-photo"},
-                "creators": [{
-                    "person_or_org": {
-                        "family_name": "Brown",
-                        "given_name": "Troy",
-                        "type": "personal"
-                    }
-                }, {
-                    "person_or_org": {
-                        "name": "Troy Inc.",
-                        "type": "organizational",
-                    },
-                }],
-                "publication_date": "2020-06-01",
-                "publisher": "Acme Inc",
-                "resource_type": {"id": "image-photo"},
-                "title": "A Romans story",
-            }
-        })
-    record = current_rdm_records_service.publish(system_identity, draft.id)
 
     current_record_communities_service.add(
         system_identity,
